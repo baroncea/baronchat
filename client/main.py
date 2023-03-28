d483a18ca845a0d592b99ec2792422fd22003dbd
Andrei-Florin Baroncea <andrei-florin.baroncea@student.tuiasi.ro>
2023-03-28 21:06:07 +0300

    cloned project from the homework repository

---
+++ client/main.py
@@ -0,0 +1,255 @@
+import sys
+import time
+import uuid
+from PyQt5.QtWidgets import QWidget, QApplication, QStackedLayout, QMessageBox
+from PyQt5.uic import loadUi
+import hashlib
+from multiprocessing.managers import BaseManager
+import multiprocessing as mp
+
+# internal queue used to manage new messages
+internal_queue = mp.Queue()
+internal_queue.put([])
+
+
+# creates a popup describing the error
+class ErrorPopup(QMessageBox):
+    def __init__(self, message):
+        super().__init__()
+        self.setIcon(QMessageBox.Critical)
+        self.setText("Error")
+        self.setInformativeText(message)
+        self.setWindowTitle("Error")
+        self.exec_()
+
+
+class QueueManager(BaseManager):
+    pass
+
+
+class Login(QWidget):
+    def __init__(self, lout):
+        super(Login, self).__init__()
+        loadUi('login.ui', self)
+        self.login_button.clicked.connect(self.login)
+        self.load_register.clicked.connect(self.goto_register)
+        self.layout = lout
+
+    def login(self):
+        print("test")
+        # gets login details
+        usr = self.username.text()
+        passwd = hashlib.md5(self.password.text().encode()).hexdigest()
+        print("test")
+        # sends them to server via message queue
+        messages = self.layout.queue.get()
+        print("test")
+        messages.append(f'SERVER {self.layout.uuid} LOGIN {usr} {passwd}')
+        print("test")
+        self.layout.queue.put(messages)
+        print("test123")
+        # wait for confirmation from the server
+        try:
+            self.layout.handle_message()
+            self.layout.self_username = usr
+            self.layout.setCurrentIndex(2)
+        except Exception as exc:
+            ErrorPopup(str(exc))
+
+    def goto_register(self):
+        self.layout.setCurrentIndex(1)
+
+
+class Register(QWidget):
+
+    def __init__(self, lout):
+        super(Register, self).__init__()
+        loadUi('register.ui', self)
+        self.register_button.clicked.connect(self.register)
+        self.load_login.clicked.connect(self.goto_login)
+        self.layout = lout
+
+    def register(self):
+        # gets login details
+        usr = self.username.text()
+        passwd = hashlib.md5(self.password.text().encode()).hexdigest()
+        # sends them to server
+        messages = self.layout.queue.get()
+        messages.append(f'SERVER {self.layout.uuid} REGISTER {usr} {passwd}')
+        self.layout.queue.put(messages)
+        # waits for confirmation from server
+        try:
+            self.layout.handle_message()
+            print("Success")
+            self.layout.self_username = usr
+            self.layout.setCurrentIndex(2)
+        except Exception as exc:
+            ErrorPopup(str(exc))
+
+    def goto_login(self):
+        self.layout.setCurrentIndex(0)
+
+
+class Chat(QWidget):
+    def __init__(self, lout):
+        super(Chat, self).__init__()
+        loadUi('chat.ui', self)
+        self.search_user.clicked.connect(self.get_user)
+        self.send_message.clicked.connect(self.send)
+        self.refresh_button.clicked.connect(self.refresh)
+        self.layout = lout
+
+    def get_user(self):
+        # checks if the chat updater is active and kills it if so
+        if self.layout.chat_updater.is_alive():
+            self.layout.chat_updater.kill()
+        # gets username
+        usr = self.receiver_username.text()
+        # asks server for messages
+        messages = self.layout.queue.get()
+        messages.append(f'SERVER {self.layout.uuid} GET_MESSAGES {usr}')
+        self.layout.queue.put(messages)
+        # handles message stream from server
+        try:
+            chat_messages = self.layout.handle_message()
+            # adds the messages to the chat window
+            self.chat_window.setPlainText('')
+            for message in chat_messages:
+                self.chat_window.append(message)
+            # sets the user as it's target for checking new messages
+            self.layout.target_username = usr
+            # empties the internal queue
+            internal_queue.get()
+            internal_queue.put([])
+            # starts a new chat updater
+            self.layout.chat_updater = ChatUpdater(self.layout)
+            self.layout.chat_updater.start()
+        except Exception as exc:
+            ErrorPopup(str(exc))
+
+    def send(self):
+        # gets target user
+        usr = self.layout.target_username
+        # if no user is targeted, shows a popup
+        if usr is None:
+            ErrorPopup("NO_TARGET")
+            return
+        # gets the message
+        msg = f'{self.layout.self_username}: {self.chat_message.text()}'
+        # updates the chat window locally
+        self.chat_window.append(msg)
+        # sends the message to the server
+        messages = self.layout.queue.get()
+        messages.append(f'SERVER {self.layout.uuid} SEND_MESSAGE {usr} {msg}')
+        self.layout.queue.put(messages)
+
+        print("Success")
+
+    def refresh(self):
+        # gets all new messages from the internal queue
+        messages = internal_queue.get()
+        for message in messages:
+            self.chat_window.append(message)
+        internal_queue.put([])
+
+    def closeEvent(self, event):
+        # signals server that the cleint is no longer connected
+        messages = self.layout.queue.get()
+        messages.append(f'SERVER {self.layout.uuid} SHUTDOWN')
+        self.layout.queue.put(messages)
+        self.layout.chat_updater.kill()
+
+
+class Layout(QStackedLayout):
+
+    def __init__(self):
+        # connects to the message queue
+        self.uuid = str(uuid.uuid4())
+        QueueManager.register('get_queue')
+        manager = QueueManager(address=('localhost', 50000), authkey=b'your_secret_key')
+        manager.connect()
+        # initializes attributes
+        self.queue = manager.get_queue()
+        self.self_username = None
+        self.target_username = None
+        self.chat_updater = ChatUpdater(self)
+        self.chat_updater.start()
+        super(Layout, self).__init__()
+        # adds the widgets to the layout
+        self.addWidget(Login(self))
+        self.addWidget(Register(self))
+        self.addWidget(Chat(self))
+
+    def handle_message(self):
+        # PROTOCOL: TARGET(CLIENT/SERVER) CLIENT_UUID COMMAND ARGS
+        while True:
+            messages = self.queue.get()
+            # checks if there are any messages
+            if len(messages) > 0:
+                print(f'received: {messages[0]}')
+                message = messages[0].split(' ', maxsplit=3)
+                # checks if the message is addressed to this instance
+                if (message[0] != 'CLIENT') or (message[1] != self.uuid):
+                    self.queue.put(messages)
+                    time.sleep(1)
+                    continue
+                # extracts message and puts the queue back
+                messages.pop(0)
+                self.queue.put(messages)
+                # message is meant for this client, handles it
+                match message[2]:
+                    case 'CONNECTED':
+                        break
+                    case 'FAILED':
+                        raise Exception(message[3])
+                    case 'MESSAGE':
+                        # gets all messages recursively
+                        remaining, msg = message[3].split(' ', maxsplit=1)
+                        ret = [msg]
+                        if remaining == '-1':
+                            return []
+                        if remaining != '0':
+                            ret += self.handle_message()
+                        return ret
+            else:
+                self.queue.put(messages)
+            time.sleep(1)
+
+
+class ChatUpdater(mp.Process):
+    def __init__(self, client_layout):
+        # gets the layout reference passed as argument
+        self.client_layout = client_layout
+        super(ChatUpdater, self).__init__()
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            messages = self.client_layout.queue.get()
+            # checks if there are any messages
+            if len(messages) > 0:
+                print(f'chat updater received: {messages[0]}')
+                message = messages[0].split(' ', maxsplit=3)
+                # checks if message is meant for this client
+                if (message[0] != 'CLIENT') or (message[1] != self.client_layout.uuid) or (message[2] != 'NEW_MESSAGE'):
+                    self.client_layout.queue.put(messages)
+                    continue
+                # message is meant for us, extract it and return queue
+                messages.pop(0)
+                self.client_layout.queue.put(messages)
+                sender_username, msg = message[3].split(' ', maxsplit=1)
+                # checks if the sender is the active chat
+                if sender_username != self.client_layout.target_username:
+                    continue
+                # puts the message in the internal queue
+                new_messages = internal_queue.get()
+                new_messages.append(f'{msg}\n')
+                internal_queue.put(new_messages)
+            else:
+                self.client_layout.queue.put(messages)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    layout = Layout()
+    sys.exit(app.exec_())
---
+++ server/main.py
@@ -0,0 +1,229 @@
+import multiprocessing as mp
+import time
+from multiprocessing.managers import BaseManager
+import os
+import sqlite3
+from collections import namedtuple
+
+# Create tuples to represent db objects
+User = namedtuple("User", ["id", "username", "password"])
+Message = namedtuple("Message", ["id", "sender_id", "receiver_id", "message"])
+
+
+# Exceptions to handle database/logging errors
+class IncorrectPasswordException(Exception):
+    pass
+
+
+class UserNotFoundException(Exception):
+    pass
+
+
+class QueueManager(BaseManager):
+    pass
+
+
+# Creates a server that hosts the message queue
+class QueueServer(mp.Process):
+    def __init__(self, message_queue):
+        self.queue = message_queue
+        self.queue.put([])
+        super(QueueServer, self).__init__()
+
+    def run(self):
+        print("Server started.")
+        QueueManager.register('get_queue', callable=lambda: self.queue)
+        manager = QueueManager(address=('localhost', 50000), authkey=b'your_secret_key')
+        sv = manager.get_server()
+        sv.serve_forever()
+
+
+# Handles database interaction
+class DatabaseManager:
+    CREATE_USERS_CMD = '''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT NOT NULL UNIQUE,
+                    password TEXT NOT NULL)'''
+    CREATE_MESSAGES_CMD = '''CREATE TABLE IF NOT EXISTS messages (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        sender_id INTEGER NOT NULL,
+                        receiver_id INTEGER NOT NULL,
+                        message TEXT NOT NULL)'''
+    INSERT_USER_CMD = '''INSERT INTO users(username, password)
+                    VALUES (?, ?)'''
+    INSERT_MESSAGE_CMD = '''INSERT INTO messages(sender_id, receiver_id, message)
+                        VALUES (?, ?, ?)'''
+    SELECT_USER_BY_USERNAME_CMD = '''SELECT * FROM users WHERE username=?'''
+    SELECT_USER_BY_ID_CMD = '''SELECT * FROM users WHERE id=?'''
+    SELECT_MESSAGES_BY_USERS_ID_CMD = '''SELECT * FROM messages WHERE (sender_id = ? AND receiver_id = ?) 
+                                    OR (sender_id = ? AND receiver_id = ?)'''
+
+    CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+    DATABASE_PATH = os.path.join(CURRENT_PATH, 'chat_db.db')
+
+    def __init__(self):
+        # Creates the tables for users and messages
+        with sqlite3.connect(self.DATABASE_PATH) as db:
+            cursor = db.cursor()
+            cursor.execute(self.CREATE_USERS_CMD)
+            cursor.execute(self.CREATE_MESSAGES_CMD)
+            cursor.close()
+
+    def get_user_by_username(self, username):
+        # returns the user object if successful
+        # if user not found, throws exception
+        with sqlite3.connect(self.DATABASE_PATH) as db:
+            cursor = db.cursor()
+            cursor.execute(self.SELECT_USER_BY_USERNAME_CMD, (username,))
+            rows = cursor.fetchall()
+            cursor.close()
+
+        if len(rows) != 0:
+            return User(*rows[0])
+        else:
+            raise UserNotFoundException("NOT_FOUND")
+
+    def get_user_by_id(self, username):
+        # returns the user object if successful
+        # if user not found, throws exception
+        with sqlite3.connect(self.DATABASE_PATH) as db:
+            cursor = db.cursor()
+            cursor.execute(self.SELECT_USER_BY_ID_CMD, (username,))
+            rows = cursor.fetchall()
+            cursor.close()
+
+        if len(rows) != 0:
+            return User(*rows[0])
+        else:
+            raise UserNotFoundException("NOT_FOUND")
+
+    def register(self, args):
+        # Inserts the new user into the db, then extracts it to get the ID and return it
+        # raises an exception if username was already used
+        with sqlite3.connect(self.DATABASE_PATH) as db:
+            cursor = db.cursor()
+            username, password = args.split(' ')
+            cursor.execute(self.INSERT_USER_CMD, (username, password))
+            cursor.close()
+        return self.get_user_by_username(username).id
+
+    def login(self, args):
+        # Gets the user by its username, then checks if the password hashes are the same
+        # Returns the ID if successful, raises exception otherwise
+        username, password = args.split(' ')
+        user = self.get_user_by_username(username)
+        if user.password == password:
+            return user.id
+        else:
+            raise IncorrectPasswordException("WRONG_PASSWORD")
+
+    def get_messages(self, requester_id, args):
+        # gets messages and returns them as a list
+        user_id = self.get_user_by_username(args).id
+        with sqlite3.connect(self.DATABASE_PATH) as db:
+            cursor = db.cursor()
+            cursor.execute(self.SELECT_MESSAGES_BY_USERS_ID_CMD, (requester_id, user_id, user_id, requester_id))
+            rows = cursor.fetchall()
+            cursor.close()
+        return [Message(*row) for row in rows]
+
+    def send_message(self, requester_id, args):
+        # saves the message into the db
+        username, message = args.split(' ', maxsplit=1)
+        user_id = self.get_user_by_username(username).id
+        with sqlite3.connect(self.DATABASE_PATH) as db:
+            cursor = db.cursor()
+            cursor.execute(self.INSERT_MESSAGE_CMD, (requester_id, user_id, message))
+            cursor.close()
+        return user_id
+
+
+class MessageHandler(mp.Process):
+    # Handles the messages
+    def __init__(self, message_queue):
+        self.message_queue = message_queue
+        self.db_manager = DatabaseManager()
+        super(MessageHandler, self).__init__()
+
+    def run(self):
+        # PROTOCOL: TARGET(CLIENT/SERVER) CLIENT_UUID COMMAND ARGS
+        print("Handler started.")
+        users = dict()
+        while True:
+            messages = self.message_queue.get()
+            # catches database/logging exceptions + empty queue
+            try:
+                print(f'received: {messages[0]}')
+                message = messages[0].split(' ', maxsplit=3)
+                # checks if message is meant for the server
+                if message[0] != 'SERVER':
+                    self.message_queue.put(messages)
+                    time.sleep(1)
+                    continue
+                # message is meant for us, extract it
+                messages.pop(0)
+                match message[2]:
+                    case 'LOGIN':
+                        # attempts to login through the database manager, may raise exception
+                        user_id = self.db_manager.login(message[3])
+                        # login successful
+                        users[message[1]] = user_id
+                        messages.append(f'CLIENT {message[1]} CONNECTED')
+
+                    case 'REGISTER':
+                        # attempts to register through the database manager, may raise exception
+                        user_id = self.db_manager.register(message[3])
+                        # register successful
+                        users[message[1]] = user_id
+                        messages.append(f'CLIENT {message[1]} CONNECTED')
+                    case 'GET_MESSAGES':
+                        # gets the messages from the database, then sends them one by one
+                        user_id = users[message[1]]
+                        chat_messages = self.db_manager.get_messages(user_id, message[3])
+                        index = len(chat_messages) - 1
+                        if index == -1:
+                            messages.append(f'CLIENT {message[1]} MESSAGE {index} EMPTY')
+                        for msg in chat_messages:
+                            messages.append(f'CLIENT {message[1]} MESSAGE {index} {msg.message}')
+                            index -= 1
+
+                    case 'SEND_MESSAGE':
+                        # saves the message into the database
+                        user_id = users[message[1]]
+                        # if user doesnt exists, raises exception
+                        user_name = self.db_manager.get_user_by_id(user_id).username
+                        receiver_id = self.db_manager.send_message(user_id, message[3])
+                        receiver_uuid = ""
+                        msg = message[3].split(' ', maxsplit=1)
+                        # alerts the receiver if his client is open
+                        if receiver_id in users.values():
+                            for uuid, rec_id in users.items():
+                                if rec_id == receiver_id:
+                                    receiver_uuid = uuid
+                                    break
+                            messages.append(f'CLIENT {receiver_uuid} NEW_MESSAGE {user_name} {msg[1]}')
+
+                    case 'SHUTDOWN':
+                        # removes user from active clients dictionary
+                        del users[message[1]]
+            except IndexError:
+                pass
+            except UserNotFoundException:
+                messages.append(f'CLIENT {message[1]} FAILED NOT_FOUND')
+            except IncorrectPasswordException:
+                messages.append(f'CLIENT {message[1]} FAILED WRONG_PASSWORD')
+            except sqlite3.IntegrityError:
+                messages.append(f'CLIENT {message[1]} FAILED ALREADY_EXISTS')
+            finally:
+                self.message_queue.put(messages)
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+    queue = mp.Queue()
+    server = QueueServer(queue)
+    handler = MessageHandler(queue)
+    handler.start()
+    server.start()
+
+
